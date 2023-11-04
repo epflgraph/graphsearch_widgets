4ea67111b8e749686130786d51d115878aae461b
Thijs van Beek <thijs@midasweb.nl>
2023-11-04 20:14:17 +0100

    refactor, load tailwind directly from node_modules

--- src/components/base/root.ts
+++ src/components/base/root.ts
@@ -1,13 +1,11 @@
 import { LitElement, unsafeCSS } from "lit";
 import { property } from "lit/decorators.js";
 
-import style from "@/shared/tailwind.global.css";
-
-const tailwind = unsafeCSS(style);
+import style from "tailwindcss/tailwind.css";
 
 export class Root extends LitElement {
   @property({ type: String })
   "locale" = "en";
 
-  static styles = [tailwind];
+  static styles = [unsafeCSS(style)];
 }
