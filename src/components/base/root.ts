10b7aa2e93f5ac5e833fdecd605bf386f44f193d
Thijs van Beek <thijs@midasweb.nl>
2023-11-29 10:57:06 +0100

    inline styles

--- src/components/base/root.ts
+++ src/components/base/root.ts
@@ -1,11 +1,12 @@
 import { LitElement, unsafeCSS } from "lit";
 import { property } from "lit/decorators.js";
 
-import style from "tailwindcss/tailwind.css";
+import tailwind from "tailwindcss/tailwind.css?inline";
+import styles from "@/styles.css?inline";
 
 export class Root extends LitElement {
   @property({ type: String })
   "locale" = "en";
 
-  static styles = [unsafeCSS(style)];
+  static styles = [unsafeCSS(styles), unsafeCSS(tailwind)];
 }
